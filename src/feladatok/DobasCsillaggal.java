665233d640aec099b9854ccdd82599250c645bf3
nedairisz <iriszneda@gmail.com>
2024-02-29 11:47:03 +0100

    ora vege

--- src/feladatok/DobasCsillaggal.java
+++ src/feladatok/DobasCsillaggal.java
@@ -3,6 +3,9 @@ package feladatok;
 
 public class DobasCsillaggal {
     
+    /* A felhasználó adja meg, h mennyit akar dobni 2 kockával.
+       A dobésok statisztikáját jelenítsük meg csillagokból álló vízszintes oszlopdiagrammal.
+       2 ****(4 db)*/
     public static void main(String[] args) {
         
     }
